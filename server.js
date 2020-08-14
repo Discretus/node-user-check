0494fe6e97c94a21018d6e0fc290d9ec0e4217db
Kirill Khramkov <hramkir@gmail.com>
2020-08-14 12:26:18 +0300

    Changed users.cpp. Restricted maximum count of users we can get. Might change it later.

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
 const users = require('bindings')('users');
 
 console.log(users.getUserNames());
-
-console.log(users.getUserNames().includes('Kiril'));
--- users.cpp
+++ users.cpp
@@ -10,7 +10,7 @@
 Napi::Array getUserNames(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
-    Napi::Array userNames;
+    Napi::Array userNames; // Массив в который будут заноситься все имена пользователей
 
     LPUSER_INFO_0 pBuf = NULL;
     LPUSER_INFO_0 pTmpBuf;
@@ -20,58 +20,51 @@ Napi::Array getUserNames(const Napi::CallbackInfo &info)
     DWORD dwTotalEntries = 0;
     DWORD dwResumeHandle = 0;
     DWORD i;
-    DWORD dwTotalCount = 0;
     NET_API_STATUS nStatus;
     LPTSTR pszServerName = NULL;
 
-    do
+    // Вызывааем NetUserEnum чтобы получить локальные учетные записи
+    nStatus = NetUserEnum((LPCWSTR)pszServerName,
+                          dwLevel,
+                          FILTER_NORMAL_ACCOUNT,
+                          (LPBYTE *)&pBuf,
+                          dwPrefMaxLen,
+                          &dwEntriesRead,
+                          &dwTotalEntries,
+                          &dwResumeHandle);
+    // Если вызов успешен
+    if ((nStatus == NERR_Success) || (nStatus == ERROR_MORE_DATA))
     {
-        nStatus = NetUserEnum((LPCWSTR)pszServerName,
-                              dwLevel,
-                              FILTER_NORMAL_ACCOUNT,
-                              (LPBYTE *)&pBuf,
-                              dwPrefMaxLen,
-                              &dwEntriesRead,
-                              &dwTotalEntries,
-                              &dwResumeHandle);
-        if ((nStatus == NERR_Success) || (nStatus == ERROR_MORE_DATA))
+        if ((pTmpBuf = pBuf) != NULL)
         {
-            if ((pTmpBuf = pBuf) != NULL)
+            userNames = Napi::Array::New(env, dwEntriesRead); // Инициализируем массив длины равной количеству полученных пользователей
+            for (i = 0; (i < dwEntriesRead); i++)             // Пробегаем по всем полученым пользователям
             {
-                for (i = 0; (i < dwEntriesRead); i++)
-                {
-                    assert(pTmpBuf != NULL);
+                assert(pTmpBuf != NULL);
 
-                    if (pTmpBuf == NULL)
-                    {
-                        fprintf(stderr, "An access violation has occurred\n");
-                        break;
-                    }
-                    pTmpBuf++;
-                    dwTotalCount++;
-                }
-                userNames = Napi::Array::New(env, dwTotalCount);
-                pTmpBuf = pBuf;
-                for (i = 0; (i < dwEntriesRead); i++)
+                if (pTmpBuf == NULL)
                 {
-                    std::wstring userNameWstr(pTmpBuf->usri0_name);
-                    std::u16string userNameU16str(wstr.begin(), wstr.end());
-                    Napi::String userName = Napi::String::New(env, userNameU16str);
-                    userNames[i] = userName;
-                    pTmpBuf++;
+                    fprintf(stderr, "An access violation has occurred\n");
+                    break;
                 }
+                // Так как Napi::String::New не принимает wchar и wstring, то сначала переводим в wstring, а потом в u16string
+                std::wstring userNameWstr(pTmpBuf->usri0_name);
+                std::u16string userNameU16str(userNameWstr.begin(), userNameWstr.end());
+                Napi::String userName = Napi::String::New(env, userNameU16str);
+                userNames[i] = userName; // Добавляем пользователя в массив
+                pTmpBuf++;
             }
         }
-        else
-            fprintf(stderr, "A system error has occurred: %d\n", nStatus);
-        if (pBuf != NULL)
-        {
-            NetApiBufferFree(pBuf);
-            pBuf = NULL;
-        }
-    } while (nStatus == ERROR_MORE_DATA);
+    }
+    // Иначе выводим системную ошибку
+    else
+        fprintf(stderr, "A system error has occurred: %d\n", nStatus);
+    // Освобождаем выделенный буфер
     if (pBuf != NULL)
+    {
         NetApiBufferFree(pBuf);
+        pBuf = NULL;
+    }
     return userNames;
 }
 
